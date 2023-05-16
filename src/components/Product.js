672fdf93494e273642004e1fee87bf967a7d6e68
Zachary <ZackCipolletta@gmail.com>
2023-05-15 21:35:49 -0700

    Added greater styling to the Product list. Product images now zoomed in to fit static size.

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import 'bootstrap/dist/css/bootstrap.css';
 import {
   Card, CardHeader, CardBody, CardFooter, Image, Stack, Heading, Text,
-  Divider, ButtonGroup, Button
+  Divider, ButtonGroup, Button, Box
 } from '@chakra-ui/react';
 
 
@@ -14,11 +14,16 @@ function Product(props) {
     < React.Fragment >
       <Card maxW='sm' onClick={() => props.whenProductClicked(props.id)}>
         <CardBody>
-          <Image src={props.imageUrl} alt={props.title} objectFit="contain" maxHeight="200px" mx="auto"/>
-          <Stack mt='6' spacing='3'>
-            <Heading size='md'>{props.title}</Heading>
-            <Text color='blue.600' fontSize='2xl'>
-              {props.price}
+        <Box
+          h="200px" // Set the desired height of the image container
+          bgImage={`url(${props.imageUrl})`} // Set the image URL as the background
+          bgSize="cover" // Zoom the image to cover the container
+          bgPosition="center" // Center the image within the container
+        />
+          <Stack mt='2' spacing='3' textAlign="left">
+            <Text >{props.title}</Text>
+            <Text fontWeight={"bold"} color='blue.600' fontSize='2xl'>
+              ${props.price}
             </Text>
           </Stack>
         </CardBody>
