9a9c154758b477a200dd9ce8693deec576d60bfa
Zachary <ZackCipolletta@gmail.com>
2023-05-31 10:10:48 -0700

    update routing in app.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import { ChakraProvider } from '@chakra-ui/react';
-import { HashRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <App />
     </ChakraProvider>
   </React.StrictMode>
 );
